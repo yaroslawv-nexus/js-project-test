eee1e67190c612a26956c10588303e3b826e8fe6
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-03 19:11:23 +0300

    final

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { getCategotyList, getBestBooks, getCategory, getBookById } from './bookAPI';
+import { onClickCategory } from './js/renderCategory';
+
+const String = "Trade Fiction Paperback";
+
+
+// getCategotyList();
+// getBestBooks();
+onClickCategory(String);
